5cc3aa1a736b48857a9d7b05224fab8f16ced5ad
hari <hari.harmalkar@gmail.com>
2018-04-09 15:09:19 +0530

    return token with user data in signup response

--- src/main/java/com/auth0/samples/authapi/user/UserController.java
+++ src/main/java/com/auth0/samples/authapi/user/UserController.java
@@ -1,13 +1,20 @@
 package com.auth0.samples.authapi.user;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+import static com.auth0.samples.authapi.security.SecurityConstants.*;
 
 @RestController
 @RequestMapping("/users")
@@ -28,7 +35,16 @@ public class UserController {
 		ApplicationUser createdUser = null;
 		try {
 			createdUser = applicationUserRepository.save(user);
-			return new ResponseEntity<ApplicationUser>(createdUser, HttpStatus.CREATED);
+			String token = Jwts.builder()
+					.setSubject(user.getEmail())
+					.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+					.signWith(SignatureAlgorithm.HS512, SECRET.getBytes())
+					.compact();
+
+			String responseContent = new String("{\"user\":{\"email\":\"" + user.getEmail() + "\"" +
+					",\"token\":\"" + token + "\"}" +
+					"}");
+			return new ResponseEntity<String>(responseContent, HttpStatus.CREATED);
 		} catch (DataIntegrityViolationException e) {
 			e.printStackTrace();
 			return new ResponseEntity<String>("{\"errors\":{\"email\" : \"This email is already taken\"}}", HttpStatus.BAD_REQUEST);
